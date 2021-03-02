8d92205887ad18fc3a3e04a77509d5d13ce8e4b5
Rizqon <m.rum.rowi.r@gmail.com>
2021-03-02 10:43:44 +0700

    add controller for testing

---
+++ app/Http/Controllers/MasterTestController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MasterTestController extends Controller
+{
+    //--- nota generator
+    public function generateNota(Request $request)
+    {
+        return "oit nota : ";
+    }
+}
