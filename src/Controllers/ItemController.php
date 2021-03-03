7c00b2c7fb2938f23fd63e7ff5685f760fc8fb55
Rizqon <m.rum.rowi.r@gmail.com>
2021-03-03 14:11:36 +0700

    update item-controller

--- src/Controllers/ItemController.php
+++ src/Controllers/ItemController.php
@@ -3,83 +3,44 @@
 namespace ArsoftModules\MasterItem\Controllers;
 
 use ArsoftModules\MasterItem\Models\Item;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ItemController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         $listItems = Item::get();
         return $listItems;
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store()
     {
-        // request()
+        $newData = new Item();
+        $newData->name = request()->name ?? '-';
+        $newData->sku = request()->sku ?? '-';
+        $newData->save();
+
+        return $newData;
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
-        //
+        return Item::find($id);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
+    public function update($id)
     {
-        //
+        $data = Item::where('id', $id)
+            ->firstOrFail();
+        $data->name = request()->name ?? '-';
+        $data->save();
+
+        return $data;
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update()
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
-        //
+        $data = Item::where('id', $id)->delete();
     }
 }
