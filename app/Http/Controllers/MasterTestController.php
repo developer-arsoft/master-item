290471bb388d149f3ee901d040d948b182e13d0e
Rizqon <m.rum.rowi.r@gmail.com>
2021-03-03 11:07:22 +0700

    updated test nota-generator

--- app/Http/Controllers/MasterTestController.php
+++ app/Http/Controllers/MasterTestController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use ArsoftModules\NotaGenerator\NotaGenerator;
+use ArsoftModules\NotaGenerator\Facades\NotaGenerator;
 use Illuminate\Http\Request;
 
 class MasterTestController extends Controller
@@ -10,11 +10,18 @@ class MasterTestController extends Controller
     // --- start: nota generator
     public function generateNota(Request $request)
     {
-        $notaGenerator = new NotaGenerator();
-        $nota = $notaGenerator->generate('item');
+        $nota = NotaGenerator::generate('orders', 'nota', 3)
+            ->addPrefix('ORDERS')
+            ->getResult();
+        
         return "generated nota " . $nota;
     }
     // --- end: nota generator
 
-
+    // --- start: master items
+    public function listItems()
+    {
+        
+    }
+    // --- end: master items
 }
